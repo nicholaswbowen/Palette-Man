8287ba2297ef511deb080646a9dadb057613a2a7
nicholaswbowen <nicholas.w.bowen@gmail.com>
2017-09-15 17:07:24 -0700

    swatch component built

--- app/src/App.js
+++ app/src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 import './styles/mainStyle.scss';
+import {Col, Grid} from 'react-bootstrap'
 import {ColorSwatch} from './colorSwatch/colorSwatch';
-import {Button} from 'react-bootstrap'
-
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <ColorSwatch color='red' />
-        <Button bsStyle='primary'>foo</Button>
-        <Button bsStyle='danger'>bar</Button>
-        <Button bsStyle='success'>baz</Button>
-
-      </div>
+      <Grid className="App">
+        <Col xs={2}>
+          <ColorSwatch color="pink" swatchHeight='125px'/>
+        </Col>
+      </Grid>
     );
   }
 }
--- app/src/colorSwatch/colorSwatch.js
+++ app/src/colorSwatch/colorSwatch.js
@@ -1,13 +1,22 @@
 import React, { PureComponent } from 'react';
+import {Well, Panel, Row, Col} from 'react-bootstrap';
+
+// pass in a color and height. get a  responsive swatch with its name.
 
 export class ColorSwatch extends PureComponent {
     render() {
-        return (
-            <div className="color-swatch-box">
-                <div className="cs-body" style={{backgroundColor: this.props.color}}>foo</div>
-                <div className="cs-title">{this.props.color}</div>
-            </div>
-        );
+        return (<Panel className="color-swatch-box" style={{height: this.props.swatchHeight}}>
+
+        <Row style={{zIndex: 0}} className="color-section">
+            <Col className="panel-content" xs={12} style={{backgroundColor: this.props.color}}></Col>
+        </Row>
+        <Row style={{zIndex: 1}} className="name-section">
+            <Col xs={12} className="panel-content">
+                <div className="text-center panel-text">{this.props.color}</div>
+            </Col>
+        </Row>
+
+        </Panel>);
     }
 }
 
